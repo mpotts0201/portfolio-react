12c46d1019d41e4825007bda2a56409b121ad8ac
mpotts0201 <mpotts0201@gmail.com>
2018-04-08 17:08:07 -0400

    refactored dom manipulation into react router dom

--- src/components/About.js
+++ src/components/About.js
@@ -16,24 +16,13 @@ border-radius: 50%;
 class About extends Component {
 
 
-// toggleAbout = () => {
-//     const element = document.getElementById('about')
-//     console.log(element)
-//     element.classList.toggle('infoSlideEnter')
-//     element.classList.toggle('infoHidden')
-// }
 
     render() {
         return (
             <div >
-                {/* <h3 onClick={()=> this.toggleAbout()}>About Me</h3> */}
-                <div className='infoHidden' id='about'>
-                {/* {this.state.aboutShow
-                ? <Img id='image' src={require("../img/headshot.jpg")} alt='A headshot of yours truly'/>
-
-                :null} */}
-                <Img id='image' className='' src={require("../img/headshot.jpg")} alt='A headshot of yours truly'/>
-                <p>My name is Murphy and I am a creator.  Creativity and building things have always been my passions, from artwork, to cooking, to full stack web applications.  I take delight in my projects and I love the satisfaction of polishing pieces and putting them together, and learning something new along the way.  </p>
+                <div className='infoSlideEnter' id='about'>
+                    <Img id='image' className='' src={require("../img/headshot.jpg")} alt='A headshot of yours truly' />
+                    <p>My name is Murphy and I am a creator.  Creativity and building things have always been my passions, from artwork, to cooking, to full stack web applications.  I take delight in my projects and I love the satisfaction of polishing pieces and putting them together, and learning something new along the way.  </p>
                 </div>
             </div>
         );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 
 class Projects extends Component {
-    // toggleProjects = () => {
-    //     const element = document.getElementById('projects')
-    //     console.log(element)
-    //     element.classList.toggle('infoSlideEnter')
-    //     element.classList.toggle('infoHidden')
-    // }
+
     render() {
         return (
             <div>
-                {/* <h3 onClick={() => this.toggleProjects()}>Projects</h3> */}
-                <div className='infoHidden' id='projects'>
+                <div className='infoSlideEnter' id='projects'>
 
                     <ul>
                         <li>Project 1</li>
