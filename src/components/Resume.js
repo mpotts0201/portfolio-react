498a89ea513d58ea90e6ef1ba5c7e249f7e672fd
mpotts0201 <mpotts0201@gmail.com>
2018-04-19 16:53:14 -0400

    added pdf of resume and styling

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import resumePNG from '../img/murphy-potts-resume.png'
+import styled from 'styled-components'
+import resumePDF from '../img/murphy-potts-resume.pdf'
+import { Document } from 'react-pdf'
+ 
+const Div = styled.div`
+display: flex;
+flex-direction: column;
+a{
+    color: white;
+    font-family: 'Roboto', sans-serif;
+}
+a:hover{
+    color: #8497ec;
+
+}
+`
+
+const H1 = styled.h1`
+font-family: 'Lato', sans-serif;
+color: white;
+span {
+font-weight: 100;
+}
+`
+
+const Img = styled.img`
+width: 250px;
+`
+
+const P = styled.p`
+color: white;
+font-family: 'Roboto', sans-serif;
+
+`
+
 
 class Resume extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Div className='infoSlideEnter info'>
+
+                <H1>Kick<span>Off</span></H1>
+                <P>My name is Murphy and I am a creator.  Creativity and building things have always been my passions, from artwork, to cooking, to full stack web applications.  Entertaining, energetic, and possessed with a passion to learn, I am looking to join creative teams that love to create as much as I do.  </P>
+                <a href={resumePDF} download>Download Resume</a>
+
+                <Document file='../img/murphy-potts-resume.pdf' />
+            </Div>
         );
     }
 }
